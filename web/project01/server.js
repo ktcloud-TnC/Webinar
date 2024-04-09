13845552c73473a297bbba3e632417544f53a533
ktcloud-TnC <ktcloud.edu@gmail.com>
2024-04-09 16:09:03 +0900

    80포트로 변경

--- web/project01/server.js
+++ web/project01/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const next = require('next');
 const loggerMiddleware = require('./middleware/logger');
 
-const port = parseInt(process.env.PORT, 10) || 3000;
+const port = parseInt(process.env.PORT, 10) || 80;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
