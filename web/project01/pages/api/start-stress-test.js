9a84de65bf3d5b3b0e9140f46b65d79448bdda02
ktcloud-TnC <ktcloud.edu@gmail.com>
2024-04-23 17:23:31 +0900

    테스트 기간 수정

--- web/project01/pages/api/start-stress-test.js
+++ web/project01/pages/api/start-stress-test.js
@@ -3,7 +3,7 @@ import { exec } from 'child_process';
 
 export default function handler(req, res) {
     // 스트레스 테스트 명령 실행
-    exec("stress-ng --cpu 1 --timeout 150", (error, stdout, stderr) => {
+    exec("stress-ng --cpu 1 --timeout 200", (error, stdout, stderr) => {
         if (error) {
             console.error('스트레스 테스트 실행 중 오류 발생:', error);
             // 오류가 발생한 경우, 단순히 상태 코드만 반환
