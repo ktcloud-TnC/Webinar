b011c75fa972613c57d694edd1189ff6cf9f2bce
ktcloud-TnC <ktcloud.edu@gmail.com>
2024-04-26 18:00:24 +0900

    무브 수정

--- was/move.sh
+++ was/move.sh
@@ -2,8 +2,8 @@
 
 mkdir -p /root/go/src/project01
 
-mv /root/webinar/was/project01 /root/go/src/
+mv /root/employee/was/project01 /root/go/src/
 
-mv /root/webinar/was/myapp.service /etc/systemd/system/
+mv /root/employee/was/myapp.service /etc/systemd/system/
 
 echo "실습파일 재배치가 완료 되었습니다."
